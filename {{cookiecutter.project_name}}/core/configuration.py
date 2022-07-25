0dc19b4ac054c7791a2814df4f0ceb2bc01b7b3d
ivan <ibodnar.dev@gmail.com>
2022-07-25 13:48:03 -0300

    config added

---
+++ {{cookiecutter.project_name}}/core/configuration.py
@@ -0,0 +1,27 @@
+from typing import Optional, Dict, Any
+
+from pydantic import BaseSettings, validator, PostgresDsn
+
+
+class Settings(BaseSettings):
+    API_V1_STR: str = '/api/v1'
+    POSTGRES_HOST: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    SQLALCHEMY_DATABASE_URI: Optional[PostgresDsn] = None
+
+    @validator("SQLALCHEMY_DATABASE_URI", pre=True)
+    def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
+        if isinstance(v, str):
+            return v
+        return PostgresDsn.build(
+            scheme="postgresql",
+            user=values.get("POSTGRES_USER"),
+            password=values.get("POSTGRES_PASSWORD"),
+            host=values.get("POSTGRES_HOST"),
+            path=f"/{values.get('POSTGRES_DB') or ''}",
+        )
+
+    class Config:
+        env_file = 'core/dev.env'
