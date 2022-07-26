e597b25ed848b74b917681833c4b34c1b3e618df
ivan <ibodnar.dev@gmail.com>
2022-07-25 21:52:17 -0300

    routes.py updated

--- {{cookiecutter.project_name}}/app/api/v1/routes.py
+++ {{cookiecutter.project_name}}/app/api/v1/routes.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
+from .handlers import {{cookiecutter.first_app}}_router
+
 
 api_router = APIRouter()
 
-api_router.include_router()
+api_router.include_router({{cookiecutter.first_app}}_router, tags={'{{cookiecutter.first_app}}'})
